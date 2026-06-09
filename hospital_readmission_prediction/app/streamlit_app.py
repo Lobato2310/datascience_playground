9a32c487f86daf2c2b1b59150c63b653cde5b6c6
Lobato2310 <lucaslobsouza@gmail.com>
2026-06-09 15:01:38 -0300

    Streamlit app and project improvements

--- hospital_readmission_prediction/app/streamlit_app.py
+++ hospital_readmission_prediction/app/streamlit_app.py
@@ -1,12 +1,49 @@
 import streamlit as st
 
 st.title("Previsão de Readmissão Hospitalar")
-st.write("Configurando o ambiente de forma simples!")
 
-nome = st.text_input("Digite seu nome:")
-idade = st.number_input("Informe sua idade:")
-tempo_hospitalizacao = st.number_input("Por quantos dias voce já esteve hospitalizado?")
-medicamentos = st.number_input("Quantos medicamentos foram receitados?")
-risco_de_readmissao = st.button("Veja a previsão de ser novamente hospitalizado")
-if nome:
-    st.write(f"")
+st.write(
+    """
+    Aplicação desenvolvida para prever a probabilidade de readmissão hospitalar utilizando Machine Learning.
+
+    Este modelo foi treinado com 16 variáveis. Para simplificar a demonstração,
+    a aplicação solicita apenas as 5 variáveis mais relevantes identificadas
+    durante a análise de importância das features.
+    """
+)
+
+idade = st.selectbox(
+    "Faixa etária",
+    ['[40-50)', '[50-60)', '[60-70)', '[70-80)', '[80-90)', '[90-100)']
+)
+
+tempo_hospitalizacao = st.number_input(
+    "Tempo de internação (dias)",
+    min_value=0
+)
+
+n_lab_procedures = st.number_input(
+    "Quantidade de exames laboratoriais",
+    min_value=0
+)
+
+n_procedures = st.number_input(
+    "Quantidade de procedimentos",
+    min_value=0
+)
+
+n_medications = st.number_input(
+    "Quantidade de medicamentos",
+    min_value=0
+)
+
+previsao = st.button("Calcular Risco de Readmissão")
+if previsao:
+    st.write("Dados recebidos:")
+    st.write({
+        "idade": idade,
+        "tempo_hospitalizacao": tempo_hospitalizacao,
+        "n_lab_procedures": n_lab_procedures,
+        "n_procedures": n_procedures,
+        "n_medications": n_medications
+    })
